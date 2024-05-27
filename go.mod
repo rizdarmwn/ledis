0e8ac6b77e72fec051364b8ff39f04f6a8f1b178
Rizdar <muhrizdar@gmail.com>
2024-05-28 01:38:23 +0700

    adding simple server and reading the RESP

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rizdarmwn/ledis
+
+go 1.22.3
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	fmt.Println("Listening on port => :6379")
+
+	l, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer conn.Close()
+
+	for {
+		resp := NewResp(conn)
+		value, err := resp.Read()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println(value)
+
+		conn.Write([]byte("+OK\r\n"))
+	}
+
+}
